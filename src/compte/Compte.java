6871ff22150768ae6fa0a2025b1246a2d4ee7e06
Giraudux <giraudux@gmail.com>
2013-12-17 14:59:14 +0100

    correction + reformat code Compte

--- src/compte/Compte.java
+++ src/compte/Compte.java
@@ -1,40 +1,46 @@
 package compte;
 
+import systeme.Systeme;
+
 import java.util.Set;
 
 /**
- * Classe compte.Compte
+ * La classe abstraite Compte représente un compte du sytème de messagerie, identifiée par une adresse unique.
  *
  * @author Alexis Giraudet, François Hallereau
  * @version 1.0
  */
 public abstract class Compte implements Comparable<Compte> {
     protected String adresse;
-    public static final String ADRESSE_CORRECTE = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$";
-    
-    /**Constructeur de la classe Compte
-     * @param adresse, l'adresse mail du compte
-     * @throws Exception, une exception est levée si elle ne correspond pas au pattern suivant : ^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$
+
+    /**
+     * Constructeur de la classe Compte.
+     *
+     * @param adresse l'adresse mail du compte
+     * @throws Exception, une exception est levée si l'adresse ne correspond pas au pattern défini par le Systeme
      */
-    public Compte(String adresse) throws Exception{
-    	if(adresse.matches(ADRESSE_CORRECTE))
-    		this.adresse = adresse;
-    	else
-    		throw new Exception("pattern de l'adresse incorrect : "+adresse);
+    public Compte(String adresse) throws Exception {
+        if (adresse.matches(Systeme.ADRESSE_CORRECTE))
+            this.adresse = adresse;
+        else
+            throw new Exception("pattern de l'adresse incorrect : " + adresse);
     }
 
-    /**Accesseur de l'attribut adresse
-     * @return l'adresse
+    /**
+     * Accesseur de l'attribut adresse.
+     *
+     * @return l'adresse du compte
      */
     public String getAdresse() {
         return adresse;
     }
 
     /**
-     * méthode de comparaison entre deux comptes
-     * La comparaison s'effectue sur l'adresse en utilisant l'ordre alphabétique 
-     * @param compte, le compte à comparer
-     * @return 1 si this > compte,-1 si this < compte et 0 sinon
+     * Méthode de comparaison entre deux comptes,
+     * La comparaison s'effectue sur l'adresse en utilisant la méthode compareTo() de la classe String.
+     *
+     * @param compte le compte à comparer
+     * @return le résultat de la comparaison des adresses (String.compareTo(String))
      */
     @Override
     public int compareTo(Compte compte) {
@@ -42,9 +48,10 @@ public abstract class Compte implements Comparable<Compte> {
     }
 
     /**
-     * redéfinition du equals
-     * @param o, un objet
-     * @return un booléen true si égal, false sinon
+     * Redéfinition du equals.
+     *
+     * @param o, un objet à comparer
+     * @return un booléen true si le compte est égal à l'Object o, false sinon
      */
     @Override
     public boolean equals(Object o) {
@@ -59,27 +66,31 @@ public abstract class Compte implements Comparable<Compte> {
     }
 
     /**
-     * renvoie une description de l'objet
-     * @return une chaine de caractère
+     * Renvoie une description de l'objet.
+     *
+     * @return une chaine de caractère représentant le compte
      */
     @Override
     public abstract String toString();
 
     /**
-     * retourne un ensemble d'utilisateurs
-     * @return une chaine de caractère
+     * Retourne l'ensemble des utilisateurs du compte.
+     *
+     * @return l'ensemble des utilisateurs du compte
      */
     public abstract Set<Utilisateur> getUtilisateurs();
 
     /**
-     * vérifie si un compte appartient ou est égal au compte
-     * @param compte, le compte à tester
-     * @return un booléen
+     * Vérifie si un compte appartient ou est égal au compte.
+     *
+     * @param compte le compte recherché
+     * @return un booléen, true si le compte a été trouvé, sinon false
      */
     public abstract boolean contient(Compte compte);
 
     /**
-     * redéfinition du hashcode de la classe compte
+     * Redéfinition du hashcode de la classe Compte.
+     *
      * @return le hashcode
      */
     @Override
